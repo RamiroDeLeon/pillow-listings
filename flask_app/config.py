b27f33593cc26948ad99cb2e9743784cea5ec0e6
Ramiro De Leon Jr <rdeleon2@umd.edu>
2020-12-15 11:48:57 -0500

    changed config file

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,4 +1,4 @@
 # Stores all configuration values
-SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
-MONGODB_HOST = "mongodb://localhost:27017/db-pf"
+SECRET_KEY = b'\xeaY\xa4\xf1(\xb8\xfc\xa03\xc5\x9b\x1a1\ry\xd8'
+# MONGODB_HOST = "mongodb://localhost:27017/db-pf"
      
