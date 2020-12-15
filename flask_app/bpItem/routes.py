6ed343f08e2a753b40ad2db748168705f4b94377
Ramiro De Leon Jr <rdeleon2@umd.edu>
2020-12-15 01:29:36 -0500

    6th

--- flask_app/bpItem/routes.py
+++ flask_app/bpItem/routes.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, url_for, redirect, request, flash
 from flask_login import current_user
 
 # from .. import movie_client
-from ..forms import MovieReviewForm, SearchForm
+# from ..forms import MovieReviewForm, SearchForm
 from ..models import User, Review, Item
 from ..utils import current_time
 
